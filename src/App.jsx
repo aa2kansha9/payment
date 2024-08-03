0e3a42336d6009f1241c0d70286ba449448166be
Aakansha Parab <aakansha.aparab@gmail.com>
2024-08-03 19:35:02 +0530

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./style"
+
+const App = () => (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+
+        </div>
+      </div>
+      
+    </div>
+)
+
+
+export default App
